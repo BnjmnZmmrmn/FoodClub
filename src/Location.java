08de4ffa60afaa637e12b9af1fa33fe6831903f1
Benjamin R. Zimmerman <bz931@berkeley.edu>
2023-03-24 17:06:04 -0700

    finished first verison

--- src/Location.java
+++ src/Location.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,6 +6,7 @@ public class Location {
 
     static final int _NUMPIRATES = 4;
     static final int _NUMCOURSES = 10;
+    static final PirateRoster pR = new PirateRoster();
 
     /** m = Meats, sl = Slushies, b = Breads, sf = Salty Foods, gf = Gross Foods, c = Candy, v = Vegestables
      * d = Dairy, n = Neggs, sm = Smoothies, p = Pizza, spf = Spicy Foods, f = Fruits*/
@@ -46,7 +46,7 @@ public class Location {
     /** m sl b sf gf c v d n sm p spf f
      *  0 1  2 3  4  5 6 7 8 9 10 11 12*/
     private int[] _FOODS = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    private ArrayList<String> _NAMES = new ArrayList<>(4);
+    private ArrayList<String> _NAMES = new ArrayList<>(_NUMPIRATES);
 
     private String _ID;
 
@@ -54,7 +54,7 @@ public class Location {
         String[] data = iData.split("\t|\n");
         _ID = data[0];
         for (int p = 0; p < _NUMPIRATES; p++) {
-            _NAMES.set(p, data[p+2]);
+            _NAMES.add(data[p+2]);
         }
         for (int c = 0; c < _NUMCOURSES; c++) {
             String vals = _COURSEVALS.get(data[c+8]);
@@ -96,5 +96,136 @@ public class Location {
                 _FOODS[12] ++;
             }
         }
+    }
+
+    private int foodVal(String fav, String weak) {
+        String[] favS = fav.split(" ");
+        String[] weakS = weak.split(" ");
+        int tot = 0;
+        for (int i = 0; i < favS.length; i ++) {
+            String val = favS[i];
+            if (val.equals("m")) {
+                tot += _FOODS[0];
+            } else if (val.equals("sl")) {
+                tot += _FOODS[1];
+            } else if (val.equals("b")) {
+                tot += _FOODS[2];
+            } else if (val.equals("sf")) {
+                tot += _FOODS[3];
+            } else if (val.equals("gf")) {
+                tot += _FOODS[4];
+            } else if (val.equals("c")) {
+                tot += _FOODS[5];
+            } else if (val.equals("v")) {
+                tot += _FOODS[6];
+            } else if (val.equals("d")) {
+                tot += _FOODS[7];
+            } else if (val.equals("n")) {
+                tot += _FOODS[8];
+            } else if (val.equals("sm")) {
+                tot += _FOODS[9];
+            } else if (val.equals("p")) {
+                tot += _FOODS[10];
+            } else if (val.equals("spf")) {
+                tot += _FOODS[11];
+            } else if (val.equals("f")) {
+                tot += _FOODS[12];
+            }
+        }
+        for (int i = 0; i < weakS.length; i ++) {
+            String val = weakS[i];
+            if (val.equals("m")) {
+                tot -= _FOODS[0];
+            } else if (val.equals("sl")) {
+                tot -= _FOODS[1];
+            } else if (val.equals("b")) {
+                tot -= _FOODS[2];
+            } else if (val.equals("sf")) {
+                tot -= _FOODS[3];
+            } else if (val.equals("gf")) {
+                tot -= _FOODS[4];
+            } else if (val.equals("c")) {
+                tot -= _FOODS[5];
+            } else if (val.equals("v")) {
+                tot -= _FOODS[6];
+            } else if (val.equals("d")) {
+                tot -= _FOODS[7];
+            } else if (val.equals("n")) {
+                tot -= _FOODS[8];
+            } else if (val.equals("sm")) {
+                tot -= _FOODS[9];
+            } else if (val.equals("p")) {
+                tot -= _FOODS[10];
+            } else if (val.equals("spf")) {
+                tot -= _FOODS[11];
+            } else if (val.equals("f")) {
+                tot -= _FOODS[12];
+            }
+        }
+        return tot;
+    }
+
+    private ArrayList<String> copy() {
+        ArrayList<String> ret = new ArrayList<String>(4);
+        for (int i = 0; i < _NUMPIRATES; i ++) {
+            ret.add(_NAMES.get(i));
+        }
+        return ret;
+    }
+
+    public String getRankings() {
+        ArrayList<String> pOne = pR.getPirate(_NAMES.get(0));
+        ArrayList<String> pTwo = pR.getPirate(_NAMES.get(1));
+        ArrayList<String> pThree = pR.getPirate(_NAMES.get(2));
+        ArrayList<String> pFour = pR.getPirate(_NAMES.get(3));
+
+        int pOneS = Integer.parseInt(pOne.get(0));
+        int pTwoS = Integer.parseInt(pTwo.get(0));
+        int pThreeS = Integer.parseInt(pThree.get(0));
+        int pFourS = Integer.parseInt(pFour.get(0));
+
+        ArrayList<Integer> strenVals = new ArrayList<Integer>(_NUMPIRATES);
+        strenVals.add(pOneS);
+        strenVals.add(pTwoS);
+        strenVals.add(pThreeS);
+        strenVals.add(pFourS);
+
+        int pOneF = foodVal(pOne.get(1), pOne.get(2));
+        int pTwoF = foodVal(pTwo.get(1), pTwo.get(2));
+        int pThreeF = foodVal(pThree.get(1), pThree.get(2));
+        int pFourF = foodVal(pFour.get(1), pFour.get(2));
+
+        ArrayList<String> names = copy();
+        ArrayList<Integer> foodVals = new ArrayList<Integer>(_NUMPIRATES);
+        foodVals.add(pOneF);
+        foodVals.add(pTwoF);
+        foodVals.add(pThreeF);
+        foodVals.add(pFourF);
+
+
+        String ret = "";
+        int count = 0;
+        while (!foodVals.isEmpty()) {
+            int tot = foodVals.get(0);
+            int ind = 0;
+            for (int i = 1; i < foodVals.size(); i ++) {
+                if (tot < foodVals.get(i)) {
+                    tot = foodVals.get(i);
+                    ind = i;
+                } else if (tot == foodVals.get(i)) {
+                    if (strenVals.get(ind) < strenVals.get(i)) {
+                        ind = i;
+                    }
+                }
+            }
+            count ++;
+            ret += count + ".";
+            ret += names.get(ind);
+            ret += "  ";
+            foodVals.remove(ind);
+            strenVals.remove(ind);
+            names.remove(ind);
+        }
+        return ret;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,76 @@
 public class Main {
     public static void main(String[] args) {
-        String shipwreck = "";
-        Location location = new Location(shipwreck);
+        String shipwreckD = "Shipwreck\n" +
+                "Pirates\n" +
+                "Ned the Skipper\tFranchisco Corvallio\n" +
+                "Bonnie Pip Culliford\tAdmiral Blackbeard\n" +
+                " \n" +
+                "Courses\n" +
+                "Ultimate Burger\tCinnamon Swirl\n" +
+                "Grapity Slush\tMustard Ice Cream\n" +
+                "Hot Cakes\tAsparagus Pie\n" +
+                "Fresh Seaweed Pie\tOrange Negg\n" +
+                "Fungi Pizza\tWild Chocomato\n" +
+                " \n";
+        String lagoonD = "Lagoon\n" +
+                "Pirates\n" +
+                "Captain Crossblades\tOl' Stripey\n" +
+                "Stuff-A-Roo\tGooblah the Grarrl\n" +
+                " \n" +
+                "Courses\n" +
+                "Broccoli and Cheese Pizza\tEye Candy\n" +
+                "Anchovies\tWild Chocomato\n" +
+                "Mallowicious Bar\tHotfish\n" +
+                "Hot Cakes\tStreaky Bacon\n" +
+                "Broccoli\tFlaming Fire Faerie Pizza\n" +
+                " \n";
+        String treasureIslandD = "Treasure Island\n" +
+                "Pirates\n" +
+                "Scurvy Dan the Blade\tSquire Venable\n" +
+                "Buck Cutlass\tLucky McKyriggan\n" +
+                " \n" +
+                "Courses\n" +
+                "Hot Tyrannian Pepper\tCinnamon Swirl\n" +
+                "Mustard Ice Cream\tJoint Of Ham\n" +
+                "Worm and Leech Pizza\tBacon Muffin\n" +
+                "Ultimate Burger\tBroccoli and Cheese Pizza\n" +
+                "Rainborific Slush\tHotfish\n" +
+                " ";
+        String hiddenCoveD = "Hidden Cove\n" +
+                "Pirates\n" +
+                "Fairfax the Deckhand\tSir Edmund Ogletree\n" +
+                "Orvinn the First Mate\tPuffo the Waister\n" +
+                " \n" +
+                "Courses\n" +
+                "Bubbling Blueberry Pizza\tRasmelon\n" +
+                "Fungi Pizza\tRainbow Negg\n" +
+                "Fish Negg\tTangy Tropic Slush\n" +
+                "Bacon Muffin\tOrange Negg\n" +
+                "Flaming Fire Faerie Pizza\tRainborific Slush\n" +
+                " \n";
+        String harpoonHarrysD = "Harpoon Harry's\n" +
+                "Pirates\n" +
+                "Federismo Corvallio\tThe Tailhook Kid\n" +
+                "Young Sproggie\tPeg Leg Percival\n" +
+                " \n" +
+                "Courses\n" +
+                "Strochal\tRainbow Negg\n" +
+                "Fish Negg\tBroccoli\n" +
+                "Ultimate Burger\tBubbling Blueberry Pizza\n" +
+                "Tangy Tropic Slush\tRainborific Slush\n" +
+                "Wriggling Grub\tOrange Negg\n" +
+                " \n";
+
+        Location shipwreck = new Location(shipwreckD);
+        Location lagoon = new Location(lagoonD);
+        Location treasureIsland = new Location(treasureIslandD);
+        Location hiddenCove = new Location(hiddenCoveD);
+        Location harpoonHarrys = new Location(harpoonHarrysD);
+
+        System.out.println("Shipwreck: " + shipwreck.getRankings());
+        System.out.println("Lagoon: " + lagoon.getRankings());
+        System.out.println("Treasure Island: " + treasureIsland.getRankings());
+        System.out.println("Hidden Cove: " + hiddenCove.getRankings());
+        System.out.println("Harpoon Harry's: " + harpoonHarrys.getRankings());
     }
 }
